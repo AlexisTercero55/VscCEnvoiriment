caa628d693028599ee26d62a699558caf6ce10e5
Alexis Tercero <alexistercero55@gmail.com>
2021-08-16 12:14:33 -0500

    first commit

---
+++ hola.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "holis" << endl;
+    cout << "holis1" << endl;
+    cout << "holis2" << endl;
+    cout << "holis3" << endl;
+
+    getchar();
+    return 0;
+}
